c49bb2ea16c862c7fc795c8e96cced321bab8583
Carloss011 <105396465+Carloss011@users.noreply.github.com>
2022-05-11 17:39:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    basic.showString("holaa")
+})
+basic.forever(function () {
+	
+})
